5d2b82e9bbdd361cd8698144b36e2a288fbb50f9
Wojciech Zieba <wojciech.zieba@protonmail.com>
2017-02-19 14:17:26 +0100

    Added background, manage main view date informations

--- TaskBook/ViewController.swift
+++ TaskBook/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dayNumberLabel: UILabel!
+    @IBOutlet weak var weekdayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "MainBackground")!)
+        
+        let date = Date()
+//        let calendar = NSCalendar.current
+//        let day = calendar.component(.day, from: date as Date)
+//        let weekday = calendar.component(.weekday,from: date as Date)
+        
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat="EEEE"
+        let weekdayString = dateFormatter.string(from: date)
+        weekdayLabel.text = weekdayString
+        
+        dateFormatter.dateFormat="d"
+        let dayString = dateFormatter.string(from: date)
+        dayNumberLabel.text = dayString
+//        dayNumberLabel.text = String(day)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
