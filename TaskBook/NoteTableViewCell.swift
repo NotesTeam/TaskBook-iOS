c8511fee031337fbb281f3831bcaad40fb64ad0e
Wojciech Zieba <wojciech.zieba@protonmail.com>
2017-02-21 19:53:44 +0100

    Showing basic information about static notes in Table View

---
+++ TaskBook/NoteTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  NoteTableViewCell.swift
+//  TaskBook
+//
+//  Created by Wojciech Zieba on 21.02.2017.
+//  Copyright © 2017 NotesTeam. All rights reserved.
+//
+
+import UIKit
+
+class NoteTableViewCell:UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TaskBook/TableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewController.swift
+//  TaskBook
+//
+//  Created by Wojciech Zieba on 21.02.2017.
+//  Copyright © 2017 NotesTeam. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController : UITableViewController {
+    
+}
